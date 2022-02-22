b3dd74e6a28754e711027cc379d01ee793ed4812
alejandra1012 <yuri1413_@hotmail.com>
2022-02-22 17:43:18 -0500

    cambioDeAlgoritmo

--- src/components/App.js
+++ src/components/App.js
@@ -9,8 +9,15 @@
 };*/
 //llamar la funcion para que funcione
 // import dataTarjetas";
+const randomize = () => {
+  const data = dataTarjetas ();
+  data.sort(() => Math.random ());
+  console.log(data);
+  
+}; 
 
 const App = (data) => {
+  const data = randomize();
   for (let i = 1; i <= 2; i++) {
     data.forEach((elemento) => {
       const card = document.createElement("div");
@@ -21,45 +28,9 @@ const App = (data) => {
     });
   }
 
-  //crear una variable global para guardar y poder
-const card = elemento.image;
-    const mix = [card.items];
-    console.log(mix)
-    for(let i = mix.length - 1; i > 0; i--){
-      const randomIndex = Math.floor(Math.random() * (i + 1));
-      const copia = mix[i];
-      mix[i] = mix[randomIndex]
-      mix[randomIndex] = copia
-    }
-    console.log(mix)
-    return card
-  };
+ 
 
 
-// const frutas = ['🍍','🍎','🍊','🍎','🍊','🍎','🍊','🍋','🍓','🍒','🍍'];
-// for(let i = frutas.length - 1; i > 0; i--){
-//   const randomIndex = Math.floor(Math.random() * (i + 1));
-//   const copia = frutas[i];
-//   frutas[i] = frutas[randomIndex]
-//   frutas[randomIndex] = copia
-// }
-// console.log(frutas)
 
 export default App;
-//src="${itemPelicula.img}"
-// `<p>${itemPelicula.id}</p>`
-// `<src="${itemPelicula.img}">`
-
-// for (variable:arreglo)
-// {
-// imprima (nombre[i])
-// i++
-// }
-
-//Ejemplo aprenderaprogramar.com
-
-//public void listarTodosLosNombres () {
-//for (String i: listaDeNombres) {
-//System.out.println (i); //Muestra cada uno de los nombres dentro de listaDeNombres
-//}
-//}
+}
