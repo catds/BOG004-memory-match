89f07aa2e2921191da7b3f1d312dbbdd9080c80a
alejandra1012 <yuri1413_@hotmail.com>
2022-02-23 11:48:58 -0500

    cambiosdeprueba

--- src/components/App.js
+++ src/components/App.js
@@ -9,15 +9,16 @@
 };*/
 //llamar la funcion para que funcione
 // import dataTarjetas";
-const randomize = () => {
+/*const randomize = () => {
   const data = dataTarjetas ();
   data.sort(() => Math.random ());
   console.log(data);
   
-}; 
+}; */
+import ghibli from '../data/ghibli/ghibli.js';
+
 
 const App = (data) => {
-  const data = randomize();
   for (let i = 1; i <= 2; i++) {
     data.forEach((elemento) => {
       const card = document.createElement("div");
@@ -28,9 +29,38 @@ const App = (data) => {
     });
   }
 
- 
+  //crear una variable global para guardar y poder
+const card = elemento.image;
+    const mix = [card.items];
+    console.log(mix)
+    for(let i = mix.length - 1; i > 0; i--){
+      const randomIndex = Math.floor(Math.random() * (i + 1));
+      const copia = mix[i];
+      mix[i] = mix[randomIndex]
+      mix[randomIndex] = copia
+    }
+    console.log(mix)
+    return card
+  };
 
+
+
+
+// const frutas = ['🍍','🍎','🍊','🍎','🍊','🍎','🍊','🍋','🍓','🍒','🍍'];
+// for(let i = frutas.length - 1; i > 0; i--){
+
+// }
+// console.log(frutas)
+
+/*const mix = [ghibli];
+for(let i = ghibli.length - 1; i > 0; i--){
+  const randomIndex = Math.floor(Math.random() * (i + 1));
+  const copia = ghibli[i];
+  ghibli[i] = ghibli[randomIndex]
+  ghibli[randomIndex] = copia
+}
+
+console.log(ghibli)*/
 
 
 export default App;
-}
