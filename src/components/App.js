6314339bc300e90b5be5119a9e7f4dfdd6847ebf
Catalina Quintero <ancquinterori@unal.edu.co>
2022-02-23 10:53:53 -0500

    El random ya funciona

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,35 @@
-// import dataTarjetas from "..main"
 
 const App = (data) => {
 
   const randomize = () => {
     const dataTarjetas = data;
-    const dataMix = [dataTarjetas];
-    dataTarjetas
     dataTarjetas.sort(() => Math.random() - 0.5)
-    return dataTarjetas
-    // console.log(dataTarjetas);
+    // return dataTarjetas
   };
   randomize();
 
-  for (let i = 1; i <= 2; i++) {
+  // for (let i = 1; i <= 2; i++) {
     data.forEach((elemento) => {
       const card = document.createElement("div");
+      const back = document.createElement("div");
       card.className = "card";
+      back.className = "back"
       const contenido = `<img src="${elemento.image}">`;
       card.innerHTML = contenido;
       document.getElementById("contenedor-cards").appendChild(card);
     });
-  }
+  // }
 
 
   // console.log(dataMix);
-  // for (let i = dataMix.length - 1; i > 0; i--) {
+  // for (let i = dataTarjetas.length - 1; i > 0; i--) {
   //   const randomIndex = Math.floor(Math.random() * (i + 1));
-  //   const copia = dataMix[i];
-  //   dataMix[i] = dataMix[randomIndex];
-  //   dataMix[randomIndex] = copia;
+  //   const copia = dataTarjetas[i];
+  //   dataTarjetas[i] = dataTarjetas[randomIndex];
+  //   dataTarjetas[randomIndex] = copia;
   // }
   // console.log(dataMix);
   // return dataMix;
-
-
 
 };
 
