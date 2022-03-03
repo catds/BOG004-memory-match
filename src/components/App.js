0a897d9bb96ef9af4335a58d173e3646c0123132
Catalina Quintero <ancquinterori@unal.edu.co>
2022-03-03 17:55:12 -0500

    actualizando rama

--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,64 @@
-const App = (data = []) =>
-{
-  const randomize = () =>
-  {
+
+
+const App = (data = []) => {
+
+  const randomize = () => {
     const dataTarjetas = data;
     dataTarjetas.sort(() => Math.random() - 0.5)
+    // return dataTarjetas
   };
   randomize();
-  let toggleCards = []
 
-  data.forEach((elemento) =>
-  {
-    const card = document.createElement("div");
-    const back = document.createElement("div");
-    card.className = "card";
-    back.className = "back";
-    back.setAttribute('name', elemento.name)
-    const contenido = `<img src="${elemento.image}">`;
-    card.innerHTML = contenido;
+  // for (let i = 1; i <= 2; i++) {
+    let toggleCards = []
 
-    card.addEventListener("click",(e)=>
-    {
+    data.forEach((elemento) => {
+      const card = document.createElement("div");
+      const back = document.createElement("div");
+      card.className = "card";
+      back.className = "back";
+      back.setAttribute('name', elemento.name)
+      const contenido = `<img src="${elemento.image}">`;
+      card.innerHTML = contenido;
+      card.addEventListener("click",(e)=>{
       back.classList.toggle("backhidden")
       checkCards(e);
-    }),
+      
+      // console.log(1,e.target)
+      })
+//borrar posicion 0 y 1 y reiniciar. borrar o un ciclo(?). Agregar siempre el alt. No spanglish
 
-    card.appendChild(back);
-    document.getElementById("contenedor-cards").appendChild(card);
-    const checkCards = (e) =>
-    {
-      const clickedCard = e.target
-      toggleCards.push(clickedCard)
-      // const toggleCards = document.querySelectorAll('.toggle');
-      // clickedCard.classList.add("toggle");
-      // console.log(toggleCards.length)
-      // console.log(e.target)
-      if(toggleCards.length===2)
-        {
+
+      card.appendChild(back);
+      document.getElementById("contenedor-cards").appendChild(card);
+      const checkCards = (e) => {
+        const clickedCard = e.target
+
+        toggleCards.push(clickedCard)
+        // const toggleCards = document.querySelectorAll('.toggle');
+        clickedCard.classList.add("toggle");
+        console.log(toggleCards.length)
+        console.log(e.target)
+        if(toggleCards.length===2){
           if(toggleCards[0].getAttribute('name')===toggleCards[1].getAttribute('name'))
           {
-            // console.log(toggleCards[0].getAttribute('name'))
-            // console.log("match");
+            console.log(toggleCards[0].getAttribute('name'))
+            console.log("match");
             toggleCards=[]
-          } else
-          {
+          } else{
             toggleCards[0].classList.toggle("backhidden")
             toggleCards[1].classList.toggle("backhidden")
             toggleCards=[]
 
-            // console.log("noMatch")
+            console.log("noMatch")
           }
         }
-    }),
-  }
+
+      }
+
+    });
+
+    // cardGenerator();
 }
+
 export default App;
