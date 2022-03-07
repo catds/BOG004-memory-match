d4a213f8c3f6bd4d2531efcb5f5605112b0be72c
Catalina Quintero <ancquinterori@unal.edu.co>
2022-03-07 10:27:36 -0500

    probando reinicio

--- src/main.js
+++ src/main.js
@@ -7,20 +7,19 @@ dataTarjetas = dataTarjetas.concat(dataTarjetas);
 console.log("Data", dataTarjetas);
 
 document.getElementById('contenedor-cards').style.display = 'none';
+document.getElementById('felicitaciones').style.display = 'none';
 function iniciarJuego() {
     document.getElementById('contenedor-cards').style.display = 'grid';
     document.getElementById('contenedor-bienvenida').style.display = 'none';
-    document.getElementById('felicitaciones').style.display = 'none';
-    
   }
   document.getElementById('iniciar juego').addEventListener('click',iniciarJuego)
   
-  document.getElementById('contenedor-bienvenida').style.display = 'none';
-  document.getElementById('contenedor-cards').style.display = 'none';
+  // document.getElementById('contenedor-bienvenida').style.display = 'none';
+  // document.getElementById('contenedor-cards').style.display = 'none';
   function reiniciar(){
-    document.getElementById('felicitaciones').style.display = 'block';
+    // document.getElementById('felicitaciones').style.display = 'block';
     document.getElementById('contenedor-bienvenida').style.display = 'block';
-    document.getElementById('contenedor-cards').style.display = 'none';
+    // document.getElementById('contenedor-cards').style.display = 'none';
 
 
   }
